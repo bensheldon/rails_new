a9b0e748478f17a154e33f9984475370736df34e
Ben Sheldon <bensheldon@gmail.com>
2018-06-18 13:50:02 -0700

    Update for Rails 5.2

--- Gemfile
+++ Gemfile
@@ -1,1 +1,5 @@
-gem 'rails', '5.1.0'
+gem 'rails', '~>5.2.0'
+
+gem 'bootsnap'
+gem 'listen'
+gem 'rspec-rails'
--- rails_template.rb
+++ rails_template.rb
@@ -1,19 +1,23 @@
 # ~/rails_template.rb
 
+gem "pry-rails"
+
 gem_group :test do
+  gem "capybara"
+  gem "factory_bot_rails"
   gem "launchy", require: false
 end
 
 gem_group :development, :test do
-  gem "capybara"
-  gem "factory_girl_rails"
-  gem "pry-rails"
   gem "rspec-rails"
+  gem "faker"
 end
 
-run_bundle
+gem_group :development do
+  gem 'annotate'
+end
 
 run "touch app/assets/stylesheets/application.scss"
 run "rm app/assets/stylesheets/application.css"
 
-generate "rspec:install"
+run "rails g rspec:install"
